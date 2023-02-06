f09c622dfddc653e9b0ffdd1adf449539c0f444d
Monirah90 <monirah_aljarrah@hotmail.com>
2023-02-06 16:44:39 +0300

    Initial Commit

---
+++ CloudKitUserName/CloudKitUserNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKitUserNameApp.swift
+//  CloudKitUserName
+//
+//  Created by Monirah aljarrah on 15/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct CloudKitUserNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
